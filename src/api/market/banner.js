37d8a22546b514865213bec7572482166878d9c0
fenghao <fengh09@chinatelecom.cn>
2024-07-24 08:39:10 +0800

    feat:banner管理完成

--- src/api/market/banner.js
+++ src/api/market/banner.js
@@ -1,8 +1,44 @@
-import request from '@/utils/request'
-export function listBanner(query){
-    return request({
-        url:'/market/banner/list',
-        method:'get',
-        params:query
-    })
+import request from "@/utils/request";
+
+// 查看banner列表
+export function listBanner(query) {
+  return request({
+    url: "/market/banner/list",
+    method: "get",
+    params: query,
+  });
+}
+
+// 查询公告详细
+export function getBanner(noticeId) {
+  return request({
+    url: "/market/banner/" + noticeId,
+    method: "get",
+  });
+}
+
+// 新增公告
+export function addBanner(data) {
+  return request({
+    url: "/market/banner",
+    method: "post",
+    data: data,
+  });
+}
+
+// 修改公告
+export function updateBanner(data) {
+  return request({
+    url: "/market/banner",
+    method: "put",
+    data: data,
+  });
+}
+
+// 删除公告
+export function delBanner(noticeId) {
+  return request({
+    url: "/market/banner/" + noticeId,
+    method: "delete",
+  });
 }
--- src/views/market/banner/index.vue
+++ src/views/market/banner/index.vue
@@ -5,9 +5,9 @@
         :model="queryParams"
         ref="queryRef"
         :inline="true"
-        label-width="68px"
+        label-width="88px"
       >
-        <el-form-item label="标题" prop="title">
+        <el-form-item label="banner标题" prop="title">
           <el-input
             v-model="queryParams.title"
             placeholder="请输入标题"
@@ -16,7 +16,7 @@
             @keyup.enter="handleQuery"
           />
         </el-form-item>
-        <el-form-item label="类型" prop="bannerType">
+        <el-form-item label="banner类型" prop="bannerType">
           <el-select
             v-model="queryParams.bannerType"
             placeholder="操作类型"
@@ -31,7 +31,7 @@
             />
           </el-select>
         </el-form-item>
-        <el-form-item label="平台" prop="platform">
+        <el-form-item label="投放平台" prop="platform">
           <el-select
             v-model="queryParams.platform"
             placeholder="操作类型"
@@ -46,22 +46,7 @@
             />
           </el-select>
         </el-form-item>
-        <el-form-item label="位置" prop="post">
-          <el-select
-            v-model="queryParams.post"
-            placeholder="操作类型"
-            clearable
-            style="width: 240px"
-          >
-            <el-option
-              v-for="dict in market_banner_post"
-              :key="dict.value"
-              :label="dict.label"
-              :value="dict.value"
-            />
-          </el-select>
-        </el-form-item>
-        <el-form-item label="状态" prop="status">
+        <el-form-item label="banner状态" prop="status">
           <el-select
             v-model="queryParams.status"
             placeholder="操作状态"
@@ -69,14 +54,14 @@
             style="width: 240px"
           >
             <el-option
-              v-for="dict in sys_common_status"
+              v-for="dict in market_banner_status"
               :key="dict.value"
               :label="dict.label"
               :value="dict.value"
             />
           </el-select>
         </el-form-item>
-        <el-form-item label="操作时间" style="width: 308px">
+        <el-form-item label="操作时间" style="width: 328px">
           <el-date-picker
             v-model="dateRange"
             value-format="YYYY-MM-DD HH:mm:ss"
@@ -101,6 +86,16 @@
       <el-row :gutter="10" class="mb8">
         <el-col :span="1.5">
           <el-button
+            type="primary"
+            plain
+            icon="Plus"
+            @click="handleAdd"
+            v-hasPermi="['market:banner:add']"
+            >新增</el-button
+          >
+        </el-col>
+        <el-col :span="1.5">
+          <el-button
             type="danger"
             plain
             icon="Delete"
@@ -123,22 +118,28 @@
           <template #default="scope">
             <el-image
               :src="scope.row.imgUrl"
-              style="width:60px"
+              style="width: 60px"
               :preview-src-list="[scope.row.imgUrl]"
             />
           </template>
         </el-table-column>
         <el-table-column label="banner类型" prop="bannerType">
           <template #default="scope">
-              <dict-tag :options="market_banner_classify" :value="scope.row.bannerType" />
+            <dict-tag
+              :options="market_banner_classify"
+              :value="scope.row.bannerType"
+            />
           </template>
         </el-table-column>
-        <el-table-column label="平台" prop="platform">
+        <el-table-column label="投放平台" prop="platform">
           <template #default="scope">
-              <dict-tag :options="market_banner_platform" :value="scope.row.platform" />
+            <dict-tag
+              :options="market_banner_platform"
+              :value="scope.row.platform"
+            />
           </template>
         </el-table-column>
-        <el-table-column label="状态" align="center" prop="status">
+        <el-table-column label="banner状态" align="center" prop="status">
           <template #default="scope">
             <el-switch
               v-model="scope.row.status"
@@ -150,7 +151,6 @@
             />
           </template>
         </el-table-column>
-
         <el-table-column
           label="操作"
           align="center"
@@ -176,29 +176,128 @@
       </el-table>
 
       <pagination
-         v-show="total > 0"
-         :total="total"
-         v-model:page="queryParams.pageNum"
-         v-model:limit="queryParams.pageSize"
-         @pagination="getList"
+        v-show="total > 0"
+        :total="total"
+        v-model:page="queryParams.pageNum"
+        v-model:limit="queryParams.pageSize"
+        @pagination="getList"
       />
 
+      <!-- 添加或者修改banner -->
+      <el-dialog :title="title" v-model="open" width="780px" append-to-body>
+        <el-form
+          ref="bannerRef"
+          :model="form"
+          :rules="rules"
+          label-width="140px"
+        >
+          <el-row>
+            <el-col :span="12">
+              <el-form-item label="banner标题" prop="title">
+                <el-input v-model="form.title" placeholder="请输入banner标题" />
+              </el-form-item>
+            </el-col>
+            <el-col :span="12">
+              <el-form-item label="banner引导图" prop="imgUrl">
+                <el-input
+                  v-model="form.imgUrl"
+                  placeholder="请输入banner引导图"
+                />
+              </el-form-item>
+            </el-col>
+            <el-col :span="12">
+              <el-form-item label="banner跳转地址" prop="skipUrl">
+                <el-input
+                  v-model="form.skipUrl"
+                  placeholder="请输入banner跳转地址"
+                />
+              </el-form-item>
+            </el-col>
+            <el-col :span="12">
+              <el-form-item label="banner详情介绍" prop="content">
+                <el-input
+                  v-model="form.content"
+                  placeholder="请输入banner详情介绍"
+                />
+              </el-form-item>
+            </el-col>
+            <el-col :span="12">
+              <el-form-item label="banner类型" prop="bannerType">
+                <el-select
+                  v-model="form.bannerType"
+                  placeholder="请选择banner类型"
+                >
+                  <el-option
+                    v-for="dict in market_banner_classify"
+                    :key="dict.value"
+                    :label="dict.label"
+                    :value="dict.value"
+                  ></el-option>
+                </el-select>
+              </el-form-item>
+            </el-col>
+            <el-col :span="12">
+              <el-form-item label="投放平台" prop="platform">
+                <el-select v-model="form.platform" placeholder="请选择投放平台">
+                  <el-option
+                    v-for="dict in market_banner_platform"
+                    :key="dict.value"
+                    :label="dict.label"
+                    :value="dict.value"
+                  ></el-option>
+                </el-select>
+              </el-form-item>
+            </el-col>
+            <el-col :span="12">
+              <el-form-item label="banner状态" prop="status">
+                <el-radio-group v-model="form.status">
+                  <el-radio
+                    v-for="dict in market_banner_status"
+                    :key="dict.value"
+                    :label="dict.value"
+                    >{{ dict.label }}</el-radio
+                  >
+                </el-radio-group>
+              </el-form-item>
+            </el-col>
+          </el-row>
+        </el-form>
+        <template #footer>
+          <div class="dialog-footer">
+            <el-button type="primary" @click="submitForm">确 定</el-button>
+            <el-button @click="cancel">取 消</el-button>
+          </div>
+        </template>
+      </el-dialog>
     </div>
   </div>
 </template>
 
 <script setup name="Bannner">
-import { listBanner } from "@/api/market/banner";
+import {
+  listBanner,
+  addBanner,
+  updateBanner,
+  delBanner,
+  getBanner
+} from "@/api/market/banner";
 const { proxy } = getCurrentInstance();
-const { market_banner_classify,market_banner_platform,market_banner_post } = proxy.useDict("market_banner_classify","market_banner_platform","market_banner_post");
+const { market_banner_classify, market_banner_platform, market_banner_status } =
+  proxy.useDict(
+    "market_banner_classify",
+    "market_banner_platform",
+    "market_banner_status"
+  );
 
 const total = ref(0);
 const ids = ref([]);
 const single = ref(true);
 const multiple = ref(true);
+const open = ref(false);
+const title = ref("");
 const loading = ref(true);
-
 const dateRange = ref([]);
+
 const data = reactive({
   form: {},
   queryParams: {
@@ -206,31 +305,91 @@ const data = reactive({
     pageSize: 10,
     title: "", //名称
     bannerType: "", //类型
-    platform:"",//平台
-    post:"",//位置
+    platform: "", //平台
+    post: "", //位置
     status: "", // 状态
+  },
+  rules: {
+    title: [{ required: true, message: "banner标题不能为空", trigger: "blur" }],
+    imgUrl: [{ required: true, message: "图片地址不能为空", trigger: "blur" }],
+    bannerType: [
+      { required: true, message: "banner类型不能为空", trigger: "change" },
+    ],
+    platform: [
+      { required: true, message: "投放平台不能为空", trigger: "change" },
+    ],
   },
 });
 
 const bannerList = ref([
-//   {
-//     id: 1,
-//     title: "首页轮播图",
-//     content: "首页轮播图",
-//     url: "http://backend-api-02.newbee.ltd/upload/20240711_14163473.png",
-//     status: 1,
-//   },
+  //   {
+  //     id: 1,
+  //     title: "首页轮播图",
+  //     content: "首页轮播图",
+  //     url: "http://backend-api-02.newbee.ltd/upload/20240711_14163473.png",
+  //     status: 1,
+  //   },
 ]);
 
-const { queryParams, form } = toRefs(data);
+const { queryParams, form, rules } = toRefs(data);
 
 // 查询banner列表
 function getList() {
   loading.value = true;
   listBanner(queryParams.value).then((response) => {
-    bannerList.value =  response.data.list;
+    bannerList.value = response.data.list;
     loading.value = false;
   });
+}
+
+/** 取消按钮 */
+function cancel() {
+  open.value = false;
+  reset();
+}
+
+/** 表单重置 */
+function reset() {
+  form.value = {
+    bannerId: undefined,
+    title: undefined,
+    imgUrl: undefined,
+    skipUrl: undefined,
+    content: "",
+    bannerType: "0",
+    platform: "0",
+    post: "0",
+    status: "0",
+  };
+  proxy.resetForm("bannerRef");
+}
+
+/** 提交按钮 */
+function submitForm() {
+  proxy.$refs["bannerRef"].validate((valid) => {
+    if (valid) {
+      if (form.value.bannerId != null) {
+        updateBanner(form.value).then((res) => {
+          proxy.$modal.msgSuccess("修改成功");
+          open.value = false;
+          getList();
+        });
+      } else {
+        addBanner(form.value).then((res) => {
+          proxy.$modal.msgSuccess("新增成功");
+          open.value = false;
+          getList();
+        });
+      }
+    }
+  });
+}
+
+/** 新增按钮操作 */
+function handleAdd() {
+  reset();
+  open.value = true;
+  title.value = "添加banner图";
 }
 
 /** 搜索按钮操作 */
@@ -243,13 +402,24 @@ function resetQuery() {
   handleQuery();
 }
 
+/**修改按钮操作 */
+function handleUpdate(row) {
+  reset();
+  const bannerId = row.bannerId || ids.value;
+  getBanner(bannerId).then(response => {
+    form.value = response.data;
+    open.value = true;
+    title.value = "修改banner";
+  });
+}
+
 /** 删除按钮操作 */
 function handleDelete(row) {
-  const bannerIds = row.id || ids.value;
+  const bannerIds = row.bannerId || ids.value;
   proxy.$modal
     .confirm('是否确认删除编号为"' + bannerIds + '"的数据项?')
     .then(function () {
-      return delRole(roleIds);
+      return delBanner(bannerIds);
     })
     .then(() => {
       getList();
@@ -260,7 +430,7 @@ function handleDelete(row) {
 
 // 多选
 function handleSelectionChange(selection) {
-  ids.value = selection.map((item) => item.roleId);
+  ids.value = selection.map((item) => item.bannerId);
   single.value = selection.length != 1;
   multiple.value = !selection.length;
 }
@@ -268,10 +438,11 @@ function handleSelectionChange(selection) {
 // banner是否启用
 function handleStatusChange(row) {
   let text = row.status === "0" ? "上架" : "下架";
+  console.log('----')
   proxy.$modal
-    .confirm('确认要' + text +'吗?')
+    .confirm("确认要" + text + "吗?")
     .then(function () {
-      return changeBannerStatus(row.roleId, row.status);
+      return updateBanner(row);
     })
     .then(() => {
       proxy.$modal.msgSuccess(text + "成功");
@@ -282,7 +453,6 @@ function handleStatusChange(row) {
 }
 
 getList();
-
 </script>
 
 <style></style>
